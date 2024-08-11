a784af645ad97384041ca1d57132de26e5f9b773
singleview-co-kr <w9721066@gmail.com>
2024-08-11 16:19:28 +0900

    Resolve #1
    new file:   assets/vendor/resizable-editor-sidebar/script.js
    new file:   assets/vendor/resizable-editor-sidebar/style.css
    modified:   functions.php

--- functions.php
+++ functions.php
@@ -19,7 +19,6 @@ define('CHILD_THEME_ASTRA_CHILD_VERSION', '1.0.0');
  */
 function child_enqueue_styles()
 {
-
     wp_enqueue_style('bootstrap-css', get_stylesheet_directory_uri() . '/assets/vendor/bootstrap/bootstrap.min.css');
     wp_enqueue_style('astra-child-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_ASTRA_CHILD_VERSION, 'all');
     wp_enqueue_style('astra-child-theme-sub-css', get_stylesheet_directory_uri() . '/assets/css/substyle.css', array('astra-theme-css'), CHILD_THEME_ASTRA_CHILD_VERSION, 'all');
@@ -90,8 +89,7 @@ include "wpCustomBlocks.php";
 include "totalSearch.php";
 
 
-function custom_products_per_page($query)
-{
+function custom_products_per_page($query) {
     if (!is_admin() && is_post_type_archive('product') && $query->is_main_query()) { // 상품 아카이브 페이지에만 적용
         $query->set('posts_per_page', 8);
     }
@@ -99,150 +97,14 @@ function custom_products_per_page($query)
 }
 add_filter('pre_get_posts', 'custom_products_per_page');
 
-
-function toast_enqueue_jquery_ui(){
-	wp_enqueue_script( 'jquery-ui-resizable');
+/**
+ * Let sidebar of post editing UX resizable
+ *
+ * @return 
+ */
+function toast_rs_enqueue(){
+    wp_enqueue_script( 'jquery-ui-resizable');
+    wp_enqueue_script( 'toast_rs_script', get_stylesheet_directory_uri() . '/assets/vendor/resizable-editor-sidebar/script.js', array('jquery-ui-resizable'), null, true);
+    wp_enqueue_style( 'toast_rs_style',  get_stylesheet_directory_uri() . '/assets/vendor/resizable-editor-sidebar/style.css');
 }
-add_action('admin_enqueue_scripts', 'toast_enqueue_jquery_ui');
-
-function toast_resizable_sidebar(){ ?>
-	<style>
-		.interface-interface-skeleton__sidebar .interface-complementary-area{width:100%;}
-		.edit-post-layout:not(.is-sidebar-opened) .interface-interface-skeleton__sidebar{display:none;}
-		.is-sidebar-opened .interface-interface-skeleton__sidebar{width:350px;}
-
-		/*UI Styles*/
-		.ui-dialog .ui-resizable-n {
-			height: 2px;
-			top: 0;
-		}
-		.ui-dialog .ui-resizable-e {
-			width: 2px;
-			right: 0;
-		}
-		.ui-dialog .ui-resizable-s {
-			height: 2px;
-			bottom: 0;
-		}
-		.ui-dialog .ui-resizable-w {
-			width: 2px;
-			left: 0;
-		}
-		.ui-dialog .ui-resizable-se,
-		.ui-dialog .ui-resizable-sw,
-		.ui-dialog .ui-resizable-ne,
-		.ui-dialog .ui-resizable-nw {
-			width: 7px;
-			height: 7px;
-		}
-		.ui-dialog .ui-resizable-se {
-			right: 0;
-			bottom: 0;
-		}
-		.ui-dialog .ui-resizable-sw {
-			left: 0;
-			bottom: 0;
-		}
-		.ui-dialog .ui-resizable-ne {
-			right: 0;
-			top: 0;
-		}
-		.ui-dialog .ui-resizable-nw {
-			left: 0;
-			top: 0;
-		}
-		.ui-draggable .ui-dialog-titlebar {
-			cursor: move;
-		}
-		.ui-draggable-handle {
-			-ms-touch-action: none;
-			touch-action: none;
-		}
-		.ui-resizable {
-			position: relative;
-		}
-		.ui-resizable-handle {
-			position: absolute;
-			font-size: 0.1px;
-			display: block;
-			-ms-touch-action: none;
-			touch-action: none;
-		}
-		.ui-resizable-disabled .ui-resizable-handle,
-		.ui-resizable-autohide .ui-resizable-handle {
-			display: none;
-		}
-		.ui-resizable-n {
-			cursor: n-resize;
-			height: 7px;
-			width: 100%;
-			top: -5px;
-			left: 0;
-		}
-		.ui-resizable-s {
-			cursor: s-resize;
-			height: 7px;
-			width: 100%;
-			bottom: -5px;
-			left: 0;
-		}
-		.ui-resizable-e {
-			cursor: e-resize;
-			width: 7px;
-			right: -5px;
-			top: 0;
-			height: 100%;
-		}
-		.ui-resizable-w {
-			cursor: w-resize;
-			width: 7px;
-			left: -5px;
-			top: 0;
-			height: 100%;
-		}
-		.ui-resizable-se {
-			cursor: se-resize;
-			width: 12px;
-			height: 12px;
-			right: 1px;
-			bottom: 1px;
-		}
-		.ui-resizable-sw {
-			cursor: sw-resize;
-			width: 9px;
-			height: 9px;
-			left: -5px;
-			bottom: -5px;
-		}
-		.ui-resizable-nw {
-			cursor: nw-resize;
-			width: 9px;
-			height: 9px;
-			left: -5px;
-			top: -5px;
-		}
-		.ui-resizable-ne {
-			cursor: ne-resize;
-			width: 9px;
-			height: 9px;
-			right: -5px;
-			top: -5px;
-		}
-	</style>
-
-	<script>
-		jQuery(window).ready(function(){
-    		setTimeout(function(){
-        		jQuery('.interface-interface-skeleton__sidebar').width(localStorage.getItem('toast_sidebar_width'))
-        		jQuery('.interface-interface-skeleton__sidebar').resizable({
-            		handles: 'w',
-            		resize: function(event, ui) {
-                		jQuery(this).css({'left': 0});
-                		localStorage.setItem('toast_sidebar_width', jQuery(this).width());
-           				}
-        		});
-    		}, 500)
-		});
-	</script>
-<?php }
-add_action('admin_head', 'toast_resizable_sidebar');
+add_action('admin_enqueue_scripts', 'toast_rs_enqueue', 20);
