788ba9ce160ed71d06cacbcad1a9b7887681faac
singleview-co-kr <w9721066@gmail.com>
2024-08-18 08:53:28 +0900

    Resolve #7
    - Implement x2board connection as a WC product attribution
    
    modified:   productFuntions.php

--- productFuntions.php
+++ productFuntions.php
@@ -10,13 +10,12 @@ function sv_prod_price_shortcode()
     $volume     = get_field('volume');
 
     if( is_object( $product ) ){
-        $weight        = $product->get_weight();
+        // $weight     = $product->get_weight();
         $attributes    = $product->get_attributes();
         $sale_price    = $product->get_sale_price();
         $regular_price = $product->get_regular_price();
     }
     else {
-        $weight        = null;
         $attributes    = null;
         $sale_price    = null;
         $regular_price = null;
@@ -45,7 +44,6 @@ function sv_prod_price_shortcode()
         <p class="sale-price"><span class="tit">할인가</span><span class="description"><?= wc_price( $sale_price ) ?></span></p>
         <?php if ($incense) : ?><p class=""><span class="tit">향</span><span class="description"><?= esc_html($incense) ?></span></p><?php endif ?>
         <?php if ($volume) : ?><p class=""><span class="tit">용량</span><span class="description"><?= $volume ?></span></p><?php endif ?>
-        <?php if ($weight) : ?><p class=""><span class="tit">무게</span><span class="description"><?= $weight ?>kg</span></p><?php endif ?>
     </div>
 
     <?php if (!empty($link_setting)) : ?>
@@ -149,6 +147,70 @@ function sv_prod_related_posts_shortcode()
         </div>
     <?php endif ?>
     <?php
+    return ob_get_clean();
+}
+
+add_shortcode('sv_prod_related_discussion', 'sv_prod_related_discussion_shortcode');
+function sv_prod_related_discussion_shortcode()
+{
+    $s_discussion_title = get_field('discussion_title');
+    $o_x2board = get_field('x2board_id');
+    $s_csv_query = get_field('csv_query');
+    $o_param = new stdClass();
+    $o_param->n_posts_per_page = get_field('list_count');
+    if( $o_x2board ) {
+        $n_board_id = $o_x2board[0]->ID;
+    }
+    else {
+        $n_board_id = 0;
+    }
+    unset($o_x2board);
+
+    // load x2board API
+    require_once X2B_PATH . 'api.php';
+    // 묻고 답하기 검색
+    $a_qna_rst = X2board\Api\get_quick_search( $n_board_id, $s_csv_query, $o_param );
+    unset($o_param);
+
+    ob_start();
+?>
+    <p class="sec-label"><?php echo $s_discussion_title != '' ? $s_discussion_title : '이 제품과 연관된 Q&A' ?></p>
+    <div id="x2board-qna-list" class="latest-qna-list">
+        <?php if( empty( $a_qna_rst ) ): ?>
+            연관된 논의가 없습니다.
+        <?php else : ?>
+            <!-- 리스트 시작 -->
+            <?php if (is_admin()) : ?>
+                <?php foreach( $a_qna_rst as $n_idx => $o_post ): ?>
+                    <div class="accordion-item">
+                        <h2><span class="q">Q.</span> <?php echo $o_post->title ?></h2>
+                        <div><?php echo nl2br($o_post->content) ?><BR><BR>
+                            <a href='<?php echo $o_post->permalink ?>'>자세히 보기</a>
+                        </div>
+                    </div>
+                <?php endforeach ?>
+            <?php else : ?>
+                <div class="accordion accordion-flush" id="accordionQna">
+                    <?php foreach( $a_qna_rst as $n_idx => $o_post ): ?>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="flush-heading<?php echo $n_idx ?>">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse<?php echo $n_idx ?>" aria-expanded="false" aria-controls="flush-collapse<?php echo $n_idx ?>">
+                                    <span class="q">Q.</span> <?php echo $o_post->title ?>
+                                </button>
+                            </h2>
+                            <div id="flush-collapse<?php echo $n_idx ?>" class="accordion-collapse collapse" aria-labelledby="flush-heading<?php echo $n_idx ?>" data-bs-parent="#accordionQna">
+                                <div class="accordion-body"><?php echo nl2br($o_post->content) ?><BR><BR>
+                                <a href='<?php echo $o_post->permalink ?>'>자세히 보기</a></div>
+                            </div>
+                        </div>
+                    <?php endforeach ?>
+                </div>
+            <?php endif ?>
+        <?php endif ?>
+        <!-- 리스트 끝 -->
+    </div>
+    
+<?php
     return ob_get_clean();
 }
 
