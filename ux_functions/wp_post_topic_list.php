f159b592a89f45ebc0146e2c2992c6baf8aa7cdb
singleview-co-kr <w9721066@gmail.com>
2024-08-25 17:09:53 +0900

    Resolve #11
    - Redraw WP post topic main
    
    modified:   admin/wp_post_category_select.php
    renamed:    assets/js/category.js -> assets/js/post_topic.js
    modified:   ux_functions/wp_post_topic_list.php

--- ux_functions/wp_post_topic_list.php
+++ ux_functions/wp_post_topic_list.php
@@ -3,68 +3,62 @@ if (! defined('ABSPATH') ) {
     exit;  // Exit if accessed directly.
 }
 
-function fw_get_blog_category_shortcode()
+function sv_get_post_topic_shortcode()
 {
-    if (!wp_script_is('fw-category-js', 'enqueued')) {
-        wp_enqueue_script('fw-category-js', get_stylesheet_directory_uri() . '/assets/js/category.js', array(), null, true);
+    if (!wp_script_is('sv-category-js', 'enqueued')) {
+        wp_enqueue_script('sv-category-js', get_stylesheet_directory_uri() . '/assets/js/post_topic.js', array(), null, true);
     }
-
-    $parent_cats = get_field('category', 326);
-    $parent_cats_2 = get_field('category_2', 326);
+	$n_post_topic_setup_page_id = 1061;
+    $a_topic_list_upper = get_field('topic_list_upper', $n_post_topic_setup_page_id);
+	$a_topic_list_lower = get_field('topic_list_lower', $n_post_topic_setup_page_id);
     ob_start();
-    if ($parent_cats) :
-        ?>
+    if ($a_topic_list_upper) : ?>
         <div class="fw-blog-category blog-category-1">
             <div class="category-tabs">
                 <div class="category-tabs-wrap">
-
-                    <?php foreach ($parent_cats as $parent_cat) :
+                    <?php foreach ($a_topic_list_upper as $n_topic_category_id) :
                         // 각 부모 카테고리에 대한 정보를 가져옵니다.
-                        $parent_cat_info = get_category($parent_cat);
-                        if ($parent_cat_info) :
-                            ?>
-                            <button class="tab" data-categoryid="<?php echo $parent_cat ?>"><?php echo $parent_cat_info->name ?></button>
-                            <?php
-                        endif;
+                        $o_topic_category_info = get_category($n_topic_category_id);
+                        if ($o_topic_category_info) : ?>
+                            <button class="tab" data-categoryid="<?php echo $n_topic_category_id ?>"><?php echo $o_topic_category_info->name ?></button>
+                        <?php endif;
                     endforeach;
+					unset($o_topic_category_info);
                     ?>
                 </div>
             </div>
             <div class="category-content">
-                <div class="blogCategorySwiper">
-                </div>
+                <div class="blogCategorySwiper"></div>
                 <div class="loading"><lottie-player src="https://lottie.host/d9e36223-6d1e-4b68-82e1-29bd9e817b9f/FjXzm2rXWa.json" background="##ffffff" speed="1" style="width: 120px; height: 120px" loop autoplay direction="1" mode="normal"></lottie-player></div>
             </div>
         </div>
-        <?php
-    endif;
-    if ($parent_cats_2) : ?>
+    <?php endif;
+	unset($a_topic_list_upper);
+    if ($a_topic_list_lower) : ?>
         <div class="fw-blog-category blog-category-2">
             <div class="category-tabs">
                 <div class="category-tabs-wrap">
-                    <?php foreach ($parent_cats_2 as $parent_cat) :
+                    <?php foreach ($a_topic_list_lower as $n_topic_category_id) :
                         // 각 부모 카테고리에 대한 정보를 가져옵니다.
-                        $parent_cat_info = get_category($parent_cat);
-                        if ($parent_cat_info) :
-                            ?>
-                            <button class="tab" data-categoryid="<?php echo $parent_cat ?>"><?php echo $parent_cat_info->name ?></button>
-                            <?php
-                        endif;
+                        $o_topic_category_info = get_category($n_topic_category_id);
+                        if ($o_topic_category_info) : ?>
+                            <button class="tab" data-categoryid="<?php echo $n_topic_category_id ?>"><?php echo $o_topic_category_info->name ?></button>
+                        <?php endif;
                     endforeach;
+					unset($o_topic_category_info);
                     ?>
                 </div>
             </div>
             <div class="category-content">
-                <div class="blogCategorySwiper">
-                </div>
+                <div class="blogCategorySwiper"></div>
                 <div class="loading"><lottie-player src="https://lottie.host/d9e36223-6d1e-4b68-82e1-29bd9e817b9f/FjXzm2rXWa.json" background="##ffffff" speed="1" style="width: 120px; height: 120px" loop autoplay direction="1" mode="normal"></lottie-player></div>
             </div>
         </div>
-        <?php
-    endif;
+    <?php endif;
+	unset($a_topic_list_lower);
     return ob_get_clean();
 }
-add_shortcode('fw_get_blog_category', 'fw_get_blog_category_shortcode');
+add_shortcode('sv_get_post_topic', 'sv_get_post_topic_shortcode');
 
 function load_subcategories()
 {
@@ -79,7 +73,7 @@ function load_subcategories()
 
     foreach ($subcategories as $category) {
         $category_link = get_category_link($category->term_id);
-        $thumbnail = get_field('category_thumbnail', 'category_' . $category->term_id);
+        $thumbnail = get_field('post_topic_thumbnail', 'category_' . $category->term_id);
         echo '<li class="swiper-slide">';
         echo '<div class="subcategory">';
         if (!empty($thumbnail)) {
