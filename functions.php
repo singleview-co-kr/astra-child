7976907663457ebc59999fc4926af5cfd1d52076
singleview-co-kr <w9721066@gmail.com>
2024-08-31 11:34:08 +0900

    Remove 403 template
    deleted:    403.php
    modified:   functions.php

--- functions.php
+++ functions.php
@@ -16,69 +16,6 @@ if (! defined('ABSPATH') ) {
  */
 define('YCX_ASTRA_CHILD_VERSION', '1.0.0');
 
-
-// add_action('wp','custom_error_pages');
-// function custom_error_pages()
-// {
-//     global $wp_query;
- 
-//     if(isset($_REQUEST['status']) && $_REQUEST['status'] == 403)
-//     {
-//         $wp_query->is_404 = FALSE;
-//         $wp_query->is_page = TRUE;
-//         $wp_query->is_singular = TRUE;
-//         $wp_query->is_single = FALSE;
-//         $wp_query->is_home = FALSE;
-//         $wp_query->is_archive = FALSE;
-//         $wp_query->is_category = FALSE;
-//         add_filter('wp_title','custom_error_title',65000,2);
-//         add_filter('body_class','custom_error_class');
-//         status_header(403);
-//         get_template_part('403');
-//         exit;
-//     }
-
-//     if(isset($_REQUEST['status']) && $_REQUEST['status'] == 401)
-//     {
-//         $wp_query->is_404 = FALSE;
-//         $wp_query->is_page = TRUE;
-//         $wp_query->is_singular = TRUE;
-//         $wp_query->is_single = FALSE;
-//         $wp_query->is_home = FALSE;
-//         $wp_query->is_archive = FALSE;
-//         $wp_query->is_category = FALSE;
-//         add_filter('wp_title','custom_error_title',65000,2);
-//         add_filter('body_class','custom_error_class');
-//         status_header(401);
-//         get_template_part('401');
-//         exit;
-//     }
-// }
- 
-// function custom_error_title($title='',$sep='')
-// {
-//     if(isset($_REQUEST['status']) && $_REQUEST['status'] == 403)
-//         return "Forbidden ".$sep." ".get_bloginfo('name');
- 
-//     if(isset($_REQUEST['status']) && $_REQUEST['status'] == 401)
-//         return "Unauthorized ".$sep." ".get_bloginfo('name');
-// }
- 
-// function custom_error_class($classes)
-// {
-//     if(isset($_REQUEST['status']) && $_REQUEST['status'] == 403)
-//     {
-//         $classes[]="error403";
-//         return $classes;
-//     }
- 
-//     if(isset($_REQUEST['status']) && $_REQUEST['status'] == 401)
-//     {
-//         $classes[]="error401";
-//         return $classes;
-//     }
-// }
-
 /* add_action('wp_loaded', 'custom_redirect_all_access_including_admin');
 function custom_redirect_all_access_including_admin() {
     $allowed_ips = array('211.202.91.71'); // 허용할 IP 주소를 여기에 입력하세요.
@@ -96,7 +33,6 @@ function custom_redirect_all_access_including_admin() {
         exit;
     }
 }*/
-
 
 /**
  * Enqueue styles
