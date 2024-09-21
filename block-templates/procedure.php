4fc25b579d79e27921f5ca4e3befc77ff2196534
singleview-co-kr <w9721066@gmail.com>
2024-09-21 11:39:06 +0900

    Commit to #11
    - Redraw WP single post; fix minor typo
    
    modified:   block-templates/procedure.php

--- block-templates/procedure.php
+++ block-templates/procedure.php
@@ -10,37 +10,32 @@ $procedure = get_field('procedure');
 <div class="sv-custom-block">
     <div class="procedure">
         <p class="sec-label"><?php echo $procedure_title != '' ? $procedure_title : '과정' ?></p>
-        <?php if (!empty($procedure)) { ?>
-            <?php if (is_admin()) : ?>
-                <?php
-                $cnt = 1;
+        <?php if (!empty($procedure)) : 
+            $cnt = 1; 
+            if (is_admin()) : 
                 foreach ($procedure as $item) : ?>
-                    <p><?php echo $cnt ?>.선택된 항목 : <strong><img src="<?php echo $item['image']['sizes']['thumbnail'] ?>" alt=""><?php echo $item['title']; ?></strong></p>
-                    <?php
-                    $cnt++;
-                endforeach; ?>
-            <?php else : ?>
+                    <p><?php echo $cnt++ ?>.선택된 항목 : <strong><img src="<?php echo $item['image']['sizes']['thumbnail'] ?>" alt=""><?php echo $item['title']; ?></strong></p>
+                <?php endforeach; 
+            else : ?>
                 <ul>
-                    <?php
-                    foreach ($procedure as $item) : ?>
-                        <li>
-                            <span class="cnt"><?php echo $cnt?></span>
-                            <div class="text-wrap">
-                                <p class="title"><?php echo $item['title']; ?></p>
-                                <?php if (!empty($item['description'])) : ?>
-                                    <p class="description"><?php echo $item['description']; ?></p>
-                                <?php endif ?>
-                            </div>
-                            <?php if (!empty($item['image'])) : ?>
-                                <div class="thumbnail">
-                                    <img src="<?php echo $item['image']['sizes']['large'] ?>" alt="">
-                                </div>
+                <?php foreach ($procedure as $item) : ?>
+                    <li>
+                        <span class="cnt"><?php echo $cnt++?></span>
+                        <div class="text-wrap">
+                            <p class="title"><?php echo $item['title']; ?></p>
+                            <?php if (!empty($item['description'])) : ?>
+                                <p class="description"><?php echo $item['description']; ?></p>
                             <?php endif ?>
-                        </li>
-                        <?php
-                    endforeach; ?>
+                        </div>
+                        <?php if (!empty($item['image'])) : ?>
+                            <div class="thumbnail">
+                                <img src="<?php echo $item['image']['sizes']['large'] ?>" alt="">
+                            </div>
+                        <?php endif ?>
+                    </li>
+                <?php endforeach; ?>
                 </ul>
-            <?php endif ?>
-        <?php } ?>
+            <?php endif;
+        endif; ?>
     </div>
 </div>
