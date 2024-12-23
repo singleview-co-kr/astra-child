d9afe3922445460c8174364598bb9e32d7a1fc53
singleview-co-kr <w9721066@gmail.com>
2024-12-23 13:20:06 +0900

    Remove deprecated code block
    modified:   block-templates/wp_custom_block.php

--- block-templates/wp_custom_block.php
+++ block-templates/wp_custom_block.php
@@ -4,8 +4,7 @@ if (! defined('ABSPATH') ) {
 }
 
 add_action('acf/init', 'hfm_acf_init_blocks');
-function hfm_acf_init_blocks()
-{
+function hfm_acf_init_blocks() {
     if (function_exists('acf_register_block_type')) {
         acf_register_block_type(
             array(
@@ -74,16 +73,3 @@ function hfm_acf_init_blocks()
     }
     // register_block_type(__DIR__ . '/block-templates/procedure_block.json');
 }
-
-function custom_qna_shortcode()
-{
-
-    $board_id = 4; // 새로 만든 게시판의 ID값으로 수정해주세요.
-    $iframe_id = uniqid();
-
-    $url = new KBUrl();
-    $_SESSION['kboard_board_id'] = $board_id;
-
-    return '<iframe id="kboard-iframe-' . $iframe_id . '" class="kboard-iframe kboard-iframe-' . $board_id . '" src="' . $url->set('kboard_id', $board_id)->set('iframe_id', $iframe_id)->toString() . '" style="width:100%" scrolling="no" frameborder="0"></iframe>';
-}
-add_shortcode('custom_qna', 'custom_qna_shortcode');
